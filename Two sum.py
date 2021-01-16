f3e95034cdef1b39831bef4f5294c24b66b8078f
geeek <hristoforov.ilya@gmail.com>
2021-01-16 12:09:45 +0300

    Docstrings

--- Two sum.py
+++ Two sum.py
@@ -1,5 +1,11 @@
+'''
+TWO SUM
+The problem: https://leetcode.com/problems/two-sum/
+'''
+
+
 ''' 
-Brute force
+Solution 1: Brute force
 Runtime: 48 ms
 Memory Usage: 14.3 MB
 Time complexity: O(n^2)
@@ -9,8 +15,9 @@ for i, inum in enumerate(nums):
 	for j, jnum in enumerate(nums[i+1:], i+1):
 		if(inum + jnum == target): return list((i, j))
 
+
 '''
-Two-pass hash table solution
+Solution 2: Two-pass hash table solution
 Runtime: 44 ms
 Memory Usage: 14.5 MB
 Time complexity: O(n), 
