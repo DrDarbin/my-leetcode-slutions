c170c08aab9caf960d5d1a7a53853c8876d145d3
geeek <hristoforov.ilya@gmail.com>
2021-01-16 16:44:19 +0300

    Two sum: leetcode class 'Solution' is added for convenience

--- Two sum.py
+++ Two sum.py
@@ -3,15 +3,16 @@ TWO SUM
 The problem: https://leetcode.com/problems/two-sum/
 '''
 
-
 ''' 
 Solution 1: Brute force
 Time complexity: O(n^2)
 Space complexity: O(1)
 '''
-for i, inum in enumerate(nums):
-	for j, jnum in enumerate(nums[i+1:], i+1):
-		if(inum + jnum == target): return list((i, j))
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+		for i, inum in enumerate(nums):
+			for j, jnum in enumerate(nums[i+1:], i+1):
+				if(inum + jnum == target): return list((i, j))
 
 
 '''
@@ -19,11 +20,13 @@ Solution 2: Two-pass hash table solution
 Time complexity: O(n), 
 Space complexity: O(n)
 '''
-hasht = {num:i for i, num in enumerate(nums)}        
-for i, num in enumerate(nums):
-	complement = target - num
-	if (complement in hasht) and (hasht[complement] != i):
-		return list((i, hasht[complement]))
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+		hasht = {num:i for i, num in enumerate(nums)}        
+		for i, num in enumerate(nums):
+			complement = target - num
+			if (complement in hasht) and (hasht[complement] != i):
+				return list((i, hasht[complement]))
 
 
 '''
@@ -31,9 +34,11 @@ Solution 3: One-pass hash table solution
 Time complexity: O(n), 
 Space complexity: O(n)
 '''
-hasht = {}
-for i, num in enumerate(nums):
-	complement = target - num
-	if (complement in hasht) and (hasht[complement] != i):
-		return list((i, hasht[complement]))
-	hasht[num] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+		hasht = {}
+		for i, num in enumerate(nums):
+			complement = target - num
+			if (complement in hasht) and (hasht[complement] != i):
+				return list((i, hasht[complement]))
+			hasht[num] = i
