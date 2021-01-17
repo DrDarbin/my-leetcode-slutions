72ae796442cd30f3c6c84dad140343b6013d3f6c
geeek <hristoforov.ilya@gmail.com>
2021-01-17 10:48:57 +0300

    1436. Destination City:solution with dictionary

--- 1436. Destination City.py
+++ 1436. Destination City.py
@@ -15,3 +15,21 @@ class Solution:
         city_to = set(city[1] for city in paths)
         for city in city_to:
             if city not in city_from: return city
+
+
+'''
+Solution 2 with Dictionary
+Time complexity: O(n)
+Space complexity: O(1)
+'''
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        city_from = {}
+        city_to = {}
+        
+        for city in paths: 
+            city_from.update({city[0]: 1}) 
+            city_to.update({city[1]: 1})
+        
+        for city in city_to:
+            if city not in city_from: return city
